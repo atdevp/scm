17990a49adb59eff299a4d8cfa89d529a5d4b819
jiangleiyu <jianglei.yu@foxmail.com>
2020-05-05 21:41:08 +0800

    delete logout interface

--- app/user/api/urls.py
+++ app/user/api/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path, re_path
 from app.user.api.rest import UsersListAPI, UsersRegisterAPI, UserDetailListAPI
 from app.user.api.rest import UserDetailUpdateAPI, UserDetailDeleteAPI, UserLoginAPI
-from app.user.api.rest import UserLogoutAPI
 
 
 urlpatterns = [
     path('register.go', UsersRegisterAPI.as_view()),
     path('login.go', UserLoginAPI.as_view()),
-    path('logout.go', UserLogoutAPI.as_view()),
     path('list.go', UsersListAPI.as_view()),
     re_path('^(?P<pk>[0-9]+)/list.go$', UserDetailListAPI.as_view()),
     re_path('^(?P<pk>[0-9]+)/update.go$', UserDetailUpdateAPI.as_view()),
