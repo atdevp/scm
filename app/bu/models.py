d9e1a648dd5ed4034643ca75c14927217f8e846a
jiangleiyu <jianglei.yu@foxmail.com>
2020-05-05 21:42:40 +0800

    add bu module

--- app/bu/models.py
+++ app/bu/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from app.user.models import UserModel
 
 # Create your models here.
+
+class BUModel(models.Model):
+    id = models.IntegerField(primary_key=True)
+    bu_name = models.CharField(max_length=100, db_index=True)
+    leader = models.ForeignKey(to='UserModel')
+    parent_id = models.IntegerField()
+
+    class Meta:
+        db_table = 'tb_bu'
+        managed = True
+    
+    def __str__(self):
+        return self.bu_name
+
